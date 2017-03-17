2045c20544cb36c1e9fecafa3a1e8e9f971ffda8
Чедуров Александр Николаевич <is641s30@awp04.csc.local>
2017-03-17 15:16:17 +0700

    prog_0.3

--- int_vector.c
+++ int_vector.c
@@ -2,54 +2,64 @@
 #include <stdlib.h>
 #include "int_vector.h"
 
-intVector* int_vector_new(size_t initial_capacity) { //sozdanie massiva
+intVector* int_vector_new (size_t initial_capacity)
+{
 	intVector *v = (intVector*)malloc(sizeof(intVector));
 	if (!v) {
+		
 		return NULL;
 	}
 	v->data = (int*)malloc(initial_capacity * sizeof(int));
 	if (!(v->data)) {
 		free(v);
+		
 		return NULL;
 	}
 	v->size = 0;
 	v->capacity = initial_capacity;
+	
 	return v;
 }
 
-size_t int_vector_get_capacity(const intVector *v) { //emkost' vectora
+size_t int_vector_get_capacity (const intVector *v)
+{
 	if (v) {
+		
 		return v->capacity;
-	}
-	else {
+	} else {
 		printf("Fail\n");
+		
 		return 0;
 	}
 }
 
-size_t int_vector_get_size(const intVector *v) { //razmer vectora
+size_t int_vector_get_size (const intVector *v)
+{
 	if (v) {
+	
 		return v->size;
-	}
-	else {
+	} else {
 		printf("Fail\n");
+	
 		return 0;
 	}
 }
 
-intVector *int_vector_copy(const intVector *v) { //ukazatel' na kopiyu vektora ili NULL
+intVector *int_vector_copy (const intVector *v)
+{
 	intVector *copy = int_vector_new(v->capacity);
 	if (!copy) {
+
 		return NULL;
-	}
-	else {
+	} else {
 		memcpy(copy->data, v->data, v->size);
+	
 		return copy;
 	}
 }
 
-int int_vector_push_back(intVector *v, int item) { //dobavlyaet element v konec
-	int x;
+int int_vector_push_back (intVector *v, int item)
+{
 	if (v->capacity == v->size) {
 		v->data = realloc(v->data, (v->capacity*2)*sizeof(int));
 		if (!v) {
@@ -57,66 +67,68 @@ int int_vector_push_back(intVector *v, int item) { //dobavlyaet element v konec
 		}
 		v->capacity *= 2;
 	}
-	x = v->size;
-	v->data[x] = item;
+	v->data[v->size] = item;
 	v->size++;
+	
 	return 0;
 }
 
-void int_vector_pop_back(intVector *v) { //udalyaet posledniy element massiva
-	int x;
+void int_vector_pop_back (intVector *v)
+{
 	if (v->size == 0) {
 		printf("No effect\n");
-	}
-	else {
-		x = v->size - 1;
-		v->data[x] = 0;
+	} else {
+		v->data[v->size-1] = 0;
 		v->size--;
 	}
 }
 
-void int_vector_free(intVector *v) { //osvobojdaet pamyat' na vector
-	if (v) {
-		free(v->data);
-		free(v);
-	}
-}
-
-int int_vector_get_item(const intVector *v, size_t index) { //vozvrashaet index-element
-	if (index > (v->size - 1))	return NULL;
-	else {
+int int_vector_get_item (const intVector *v, size_t index)
+{
+	if (index > (v->size - 1)) {
+		return NULL;
+	} else {
 		int i = index;
 		int x = v->data[i];
+		
 		return x;
 	}
 }
 
-void int_vector_set_item(intVector *v, size_t index, int item) { //prisvaivaet index-elementu zna4enie item
+void int_vector_set_item (intVector *v, size_t index, int item)
+{
 	if (!(index > (v->size - 1))) v->data[index] = item;
 }
 
-int int_vector_shrink_to_fit(intVector *v) { //umen'shaet emkost' do razmera
+int int_vector_shrink_to_fit (intVector *v)
+{
 	v = realloc(v, v->size);
-	if (v->capacity == v->size) return 0;
+	if (v->capacity == v->size) {
+		return 0;
+	}
 	else return -1;
 }
 
-int int_vector_reserve(intVector *v, size_t new_capacity) { //menyaet emkost'
-	if (new_capacity <= v->capacity) return -1;
-	else {
+int int_vector_reserve (intVector *v, size_t new_capacity)
+{
+	if (new_capacity <= v->capacity) {
+		
+		return -1;
+	} else {
 		v = realloc(v, new_capacity);
 		v->capacity = new_capacity;
+	
 		return 0;
 	}
 }
 
-int int_vector_resize(intVector *v, size_t new_size) { //menyaet razmer
-	int x = 0;
+int int_vector_resize (intVector *v, size_t new_size)
+{
+	int x;
 	if (v->size > new_size) {
 		v->size = new_size;
 		x = int_vector_shrink_to_fit(v);
-	}
-	else {
+	} else {
 		x = int_vector_reserve(v, new_size);
 		for (int i = v->size; i < new_size; i++) {
 			//x = int_vector_push_back(v, 0);
@@ -125,5 +137,14 @@ int int_vector_resize(intVector *v, size_t new_size) { //menyaet razmer
 		}
 		v->size = new_size;
 	}
+	
 	return x;
+}
+
+void int_vector_free (intVector *v)
+{
+	if (v) {
+		free(v->data);
+		free(v);
+	}
 }
--- main.c
+++ main.c
@@ -1,46 +1,48 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include "int_vector.h"
 
 int main() {
 	int x, item, i, j, new_size;
-	printf("Vvedite razmer: ");
+	printf("Input capacity: ");
 	scanf("%d", &x);
 	intVector *a;
-	a = int_vector_new(x);
-	printf("\n Emkost': %zd", int_vector_get_capacity(a));
+	a = int_vector_new (x);
+	printf("\nSize: %zd\n", int_vector_get_size (a));
+	printf("Capacity: %zd\n", int_vector_get_capacity (a));
 
-	intVector *b;
-	b = int_vector_copy(a);
-
-	printf("\n Vvedite element: ");
+	//intVector *b;
+	//b = int_vector_copy (a);
+	
+	printf("\nInput element: ");
 	scanf("%d", &item);
-	x = int_vector_push_back(a, item);
-	printf("\n Vector a (posle dobavleniya elementa): ");
+	x = int_vector_push_back (a, item);
+	printf("\nVector a (+ 1 element): ");
 	for (i = 0; i < a->size; i++) {
-		j = int_vector_get_item(a, i);
-		printf("%d  ", j);
+		j = int_vector_get_item (a, i);
+		printf("%d ", j);
 	}
-	printf("\n Razmer: %zd\n", int_vector_get_size(a));
-
-	int_vector_pop_back(a);
-	printf("\n Vector a (posle udaleniya elementa): ");
+	printf("\nSize: %zd\n", int_vector_get_size (a));
+	printf("Capacity: %zd\n", int_vector_get_capacity (a));
+	int_vector_pop_back (a);
+	printf("\nVector a (- 1 element): ");
 	for (i = 0; i < a->size; i++) {
-		j = int_vector_get_item(a, i);
-		printf("%d  ", j);
+		j = int_vector_get_item (a, i);
+		printf("%d ", j);
 	}
-	printf("\n Razmer: %zd\n", int_vector_get_size(a));
-
-	printf("\n Vvedite noviy razmer: ");
+	printf("\nSize: %zd\n", int_vector_get_size (a));
+	printf("Capacity: %zd\n", int_vector_get_capacity (a));
+	
+	printf("\nInput new capacity: ");
 	scanf("%d", &new_size);
-	x = int_vector_resize(a, new_size);
-	printf("\n Vector a: ");
+	x = int_vector_reserve (a, new_size);
+	printf("\nInput new size: ");
+	scanf("%d", &new_size);
+	x = int_vector_resize (a, new_size);
+	printf("\nVector a: ");
 	for (i = 0; i < a->size; i++) {
-		j = int_vector_get_item(a, i);
-		printf("%d  ", j);
+		j = int_vector_get_item (a, i);
+		printf("%d ", j);
 	}
-	printf("\n Razmer: %zd\n", int_vector_get_size(a));
-	printf("\n Emkost': %zd\n", int_vector_get_capacity(a));
-	//return 0;
-	_getch();
+	
+	return 0;
 }
