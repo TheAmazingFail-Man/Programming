c1520c3ef62e99fade4c65f44864528e946a1e49
Чедуров Александр Николаевич <is641s30@lwp01.csc.local>
2017-04-01 11:51:12 +0600

    HELLYEAH

--- 3lab/src/main.c
+++ 3lab/src/main.c
@@ -7,12 +7,17 @@
 
 int main(int argc, char *argv[])
 {	
+	int i, x = 0;
 	uint32_t numbers;
 	
 	FILE *begin = fopen(argv[1], "r");
 	FILE *end = fopen(argv[2], "w");
-	fscanf(begin, "%x\n", &numbers);
-	fprintf(end, "%x", numbers);
+	for (i = 0; i < 3; i++) {
+		fscanf(begin, "%x\n", &numbers);
+		//fprintf(end, "%x\n", numbers);
+		x++;
+	}
+	printf("%d", x);
 	fclose(begin);
 	fclose(end);
 	
